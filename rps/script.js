5cae41305d9ce0707d03728a00c78833b9741283
Harry Robertson <harryrobbo77@outlook.com>
2024-01-16 17:59:24 +0000

    finished making the basis of the game

--- rps/script.js
+++ rps/script.js
@@ -1,9 +1,53 @@
-const a = "Rock"
-const b = "Paper" 
-const c = "Scissors" 
-
 function getComputerChoice() {
-    return a 
+    let choices = ["Rock", "Paper", "Scissors"]
+    return choices [Math.floor(Math.random() * choices.length)]
 }
+ 
+
+function playerSelection() {
+    return prompt(String("Rock, Paper or Scissors"))
+}
+
+function playRound(playerSelection, getComputerChoice) { 
+
+
+if ((playerSelection == "Rock" && getComputerChoice == "Scissors") || 
+    (playerSelection == "Paper" && getComputerChoice == "Rock") || 
+    (playerSelection == "Scissors" && getComputerChoice == "Paper")) {
+    console.log(playerSelection + " beats " + getComputerChoice + " ,You Win! "); 
+
+} else if ((playerSelection == "Rock" && getComputerChoice == "Paper") || 
+    (playerSelection == "Paper" && getComputerChoice == "Scissors") || 
+    (playerSelection == "Scissors" && getComputerChoice == "Rock")) {
+    console.log(getComputerChoice + " beats " + playerSelection + " ,You Lose! ");
+
+} else if (playerSelection == getComputerChoice) 
+    console.log(playerSelection + " is the same as " + getComputerChoice + " ,It's a Draw ") }
+
+
+
+
+playRound(playerSelection(), getComputerChoice())
+
+
+
+
+
+
+    
+
+
+
+
+    
+
+
+
+   
+
+
+
+
+
 
 
