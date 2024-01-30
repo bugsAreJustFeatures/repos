6697154002a8303e0afb6a3f5ab7745a6ba43bf1
Harry Robertson <harryrobbo77@outlook.com>
2024-01-30 17:12:30 +0000

    added scoreboard but scoring does not change

--- rps/script.js
+++ rps/script.js
@@ -10,29 +10,50 @@ function playerSelection() {
 
 function playRound(playerSelection, getComputerChoice) { 
 
+    let computerPoint = ""
+    let humanPoint = ""
+
 
 if ((playerSelection == "rock" && getComputerChoice == "scissors") || 
     (playerSelection == "paper" && getComputerChoice == "rock") || 
-    (playerSelection == "scissors" && getComputerChoice == "paper")) {
-    console.log(playerSelection + " beats " + getComputerChoice + " ,You Win! "); 
+    (playerSelection == "scissors" && getComputerChoice == "paper")) {          
+    
+    console.log(playerSelection + " beats " + getComputerChoice + " , You Win! ");
+    humanPoint++ ;
+
 
 } else if ((playerSelection == "rock" && getComputerChoice == "paper") || 
     (playerSelection == "paper" && getComputerChoice == "scissors") || 
     (playerSelection == "scissors" && getComputerChoice == "rock")) {
-    console.log(getComputerChoice + " beats " + playerSelection + " ,You Lose! ");
+
+    console.log(getComputerChoice + " beats " + playerSelection + " , You Lose! ");    
+    computerPoint++ ;
 
 } else if (playerSelection == getComputerChoice) {
-    console.log(playerSelection + " is the same as " + getComputerChoice + " ,It's a Draw ") 
+    console.log("Both players selected " + playerSelection  + " , It's a Draw. Play Again ") 
 
-} else console.log("An Error Has Occured, Please check your answer and play again.")
+} else console.log("Please check your answer and play again.");
 }
 
+// playRound(playerSelection(), getComputerChoice())
 
+function game() {
 
+    let humanPoint = 0
+    let computerPoint = 0
 
+    playRound(playerSelection(), getComputerChoice())
+    playRound(playerSelection(), getComputerChoice())
+    playRound(playerSelection(), getComputerChoice())
+    playRound(playerSelection(), getComputerChoice())
+    playRound(playerSelection(), getComputerChoice())
 
-playRound(playerSelection(), getComputerChoice())
+    console.log("You  " + humanPoint + " : " + computerPoint + "  Computer")
 
+    
+}
+
+game()
 
 
 
