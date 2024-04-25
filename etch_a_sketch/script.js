7335715fb12a0ae4cba81f20afddb1e8458a6116
Harry Robertson <harryrobbo77@outlook.com>
2024-04-25 18:47:30 +0100

    complete etch-a-sketch assignment

--- etch_a_sketch/script.js
+++ etch_a_sketch/script.js
@@ -1,27 +1,59 @@
-const flexContainer = document.createElement('div');
-flexContainer.id = "flexContainer";
-document.body.appendChild(flexContainer);
+// functions for making the grid
 
-// const divs = document.createElement('div');
-// divs.id = "divs";
-// document.getElementById('flexContainer').appendChild(divs);
-
-const cell = document.createElement('div');
-cell.className = "cell";
-document.getElementById("flexContainer").appendChild(cell);
-
+// function for creating each div
 function createRow(num) {
-    for (let i = 0; i < (num * num); i++) {
-        let eachRow = document.createElement('div');
-        eachRow.className = "eachRow";
-        eachRow.style.border = "thick dotted black";
-        eachRow.style.padding = "10px"; 
-        eachRow.style.width = "calc(100% / 16)";
-        eachRow.style.height = "calc(100% / 16)";
-        document.getElementById('flexContainer').appendChild(eachRow);
+    for (let i = (num - 1); i < num; i++) {
+        let newRow = document.createElement("div");
+        newRow.className = "column";
+        newRow.addEventListener("mouseover", changeColour);
+        document.getElementsByClassName("row")[0].appendChild(newRow);
     }
 };
+// function that creates the whole grid
+    function createGrid(n) {
+        for (let i = 0; i < n; i++) {
+            createRow(n);
+        }
+    }
+
+    // function for creating a new grid with the userInput
+    function newGrid() {
+        let userInput = prompt("Please enter a number between 0 and 100. ");
+
+        if ((userInput < 1) || (userInput > 99) || (isNaN(userInput))) {
+            alert("Please check you entered a number between 0 and 100. ")
+        } else {
+            createGrid(userInput)
+        }
+    };
+
+// function for squares to change colour
+function changeColour() {
+    // Check if the clicked div is in the inner area (not on the sides)
+    if (!this.classList.contains("column-side")) {
+        this.style.backgroundColor = "white"; // Change background color of the clicked div
+    }
+}
+// function for resetting colour of divs when square is renewed
+function resetColour() {
+    let elements = document.querySelectorAll(".column");
+    elements.forEach(function(element) {
+        element.style.backgroundColor = "black";
+    });
+}
 
 
-createRow(16);
-// createColumn(16);
+// button
+
+// reset button
+const resetButton = document.getElementById("resetBtn");
+
+// event for reseting colour to default
+resetButton.addEventListener("click", resetColour);
+
+// event for creating a new grid to the user's discretion
+resetButton.addEventListener("click", newGrid);
+
+
+// // function that creates the basic 16x16 grid
+// createGrid(16)
