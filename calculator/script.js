1630deb5d665836d8ac30b5675b32c8d5ca66ad2
Harry Robertson <harryrobbo77@outlook.com>
2024-05-10 20:05:07 +0100

    improved design and done basic eventListeners

--- calculator/script.js
+++ calculator/script.js
@@ -1,13 +1,55 @@
+// -----DOM--- //
+
+function enterNums(x, y) {
+    document.getElementById("answerBox").textContent = x;
+    
+} 
+
+document.getElementById("n1").addEventListener("click", () => {
+    enterNums("1");
+});
+document.getElementById("n2").addEventListener("click", () => {
+    enterNums("2");
+});
+document.getElementById("n3").addEventListener("click", () => {
+    enterNums("3");
+});
+document.getElementById("n4").addEventListener("click", () => {
+    enterNums("4");
+});
+document.getElementById("n5").addEventListener("click", () => {
+    enterNums("5");
+});
+document.getElementById("n6").addEventListener("click", () => {
+    enterNums("6");
+});
+document.getElementById("n7").addEventListener("click", () => {
+    enterNums("7");
+});
+document.getElementById("n8").addEventListener("click", () => {
+    enterNums("8");
+});
+document.getElementById("n9").addEventListener("click", () => {
+    enterNums("9");
+});
+document.getElementById("n0").addEventListener("click", () => {
+    enterNums("0");
+});
+document.getElementById("decimal").addEventListener("click", () => {
+    enterNums(".");
+});
 
 
-function num1(number1) {
-    return number1;
-}
 
 
-function num2(number2) {
-    return number2;
-}
+
+
+
+
+
+
+
+// ------Logic-------- //
 
 //operator functions
 function add(num1, num2) {
@@ -34,5 +76,6 @@ function operate(a, b, sign) {
         return divide(a, b)
     };
 };
+
 
 
