44336b2c187faec2b9f71eef0bd4f3cdd141b3a7
hxcrry <harryrobbo77@outlook.com>
2024-07-26 21:35:26 +0100

    made template html and done basic setup for webpack

---
+++ projects/restaurantPage/webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                title: "Restaurant Page",
+                template: "src/template.html",
+
+            }
+        )
+    ]
+};
