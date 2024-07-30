64451315fd79260433f4027b08b5a9f0f6451cad
hxcrry <harryrobbo77@outlook.com>
2024-07-30 22:19:44 +0100

    done all pages, just need to link pages and buttons, and title to home

--- projects/restaurantPage/src/contactPage.js
+++ projects/restaurantPage/src/contactPage.js
@@ -32,7 +32,19 @@ title.addEventListener("click", () => {
 
 console.log("This is the contact page"); // testing pages loading correctly
 
+const contactContent = document.createElement("div");
+contactContent.classList.add("contactContent");
+mainContent.appendChild(contactContent);
 
+const contactInfo = document.createElement("div");
+contactInfo.classList.add("contactInfo");
+contactInfo.innerHTML = "<u>Telephone:</u> &nbsp;&nbsp;+44 01234 567890 <br><br><br> <u>Email:</u> &nbsp;&nbsp;Info@FoodHaven.co.uk <br><br><br> <u>Location:</u> &nbsp;&nbsp;No. 10 Downing Street, London, England"
+contactContent.appendChild(contactInfo);
+
+const contactImg = document.createElement("img");
+contactImg.classList.add("contactImg")
+contactImg.src = "https://d.ibtimes.co.uk/en/full/1386743/google-maps-london-underground-logo.png?w=1600&h=1200&q=88&f=4e3b79ac62ed993b702a55d723c38ea5";
+contactContent.appendChild(contactImg);
 
 
 
