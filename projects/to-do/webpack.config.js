82bb7ca71c5f8db6a9069e517a3d010e308b6712
hxcrry <harryrobbo77@outlook.com>
2024-08-05 20:45:56 +0100

    linked webpack and got it ready, with font that i want to use for project

---
+++ projects/to-do/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: {
+      index: "./src/index.js",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Index",
+            filename: "Index.html",
+            template: "src/index.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+              test: /\.css$/,
+              use: [
+                { loader: 'style-loader' },
+                { loader: 'css-loader' },
+              ],
+            },
+          ],
+    },
+};
