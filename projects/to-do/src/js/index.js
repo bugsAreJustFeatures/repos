8973cd32906907be36f0a7039ee9de4fb3485c03
hxcrry <harryrobbo77@outlook.com>
2024-08-12 14:02:10 +0100

    done task adding functionality and priority colour coding, now i need to do edit button and delete button

--- projects/to-do/src/js/index.js
+++ projects/to-do/src/js/index.js
@@ -1,12 +1,40 @@
 import "../css/style.css";
 
-
 const taskContainer = document.getElementById("taskContainer");
 const newTaskBtn = document.getElementById("newTaskBtn");
+const dialog = document.getElementById("taskDialog");
+const confirmBtn = document.getElementById("dialogConfirmBtn");
+const cancelBtn = document.getElementById("dialogCancelBtn");
+const taskNameInput = document.getElementById("dialogTaskName");
+const taskPriorityInput = document.getElementById("dialogTaskPriority");
 
-newTaskBtn.addEventListener("click", createTask);
+let userName;
+let userPriority;
+
+newTaskBtn.addEventListener("click", () => {
+    dialog.style.display = "block";
+    dialog.showModal();
+});
+
+confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault()
+    dialog.close();
+    createTask();
+    dialog.style.display = "none";
+    console.log("confirmBtnEvent  " + userName)
+    console.log("confirmBtnEvent  " + userPriority)
+})
+
+cancelBtn.addEventListener("click", () => {
+    dialog.style.display = "none";
+    dialog.close()
+})
 
 function createTask() {
+
+    userName = String(taskNameInput.value);
+    userPriority = String(taskPriorityInput.value);
+
     //-----container for tasks-----//
     let task = document.createElement("div");
     task.classList.add("task");
@@ -14,7 +42,7 @@ function createTask() {
 
     let taskName = document.createElement("div");
     taskName.classList.add("taskName");
-    taskName.innerHTML = "This is a task";
+    taskName.innerHTML = userName;
     task.appendChild(taskName);
 
     let taskIcons = document.createElement("div");
@@ -32,7 +60,22 @@ function createTask() {
     taskPriorityImg.classList.add("taskPriorityImg");
     taskPriorityImg.src = "http://127.0.0.1:5500/src/img/alert-circle-outline.png";
     taskPriorityImg.alt = "Task_Priority_Icon"
+    taskPriority.style.backgroundColor = "white";
+
     taskPriority.appendChild(taskPriorityImg);
+
+    console.log("DOMevent  " + userName)
+    console.log("DOMevent " + userPriority)
+
+    if (userPriority === "default") {
+        taskPriority.style.backgroundColor == "blue"
+    } else if (userPriority === "Low") {
+        taskPriority.style.backgroundColor = "lime"
+    } else if (userPriority === "Medium") {
+        taskPriority.style.backgroundColor = "orange"
+    } else if (userPriority === "High") {
+        taskPriority.style.backgroundColor = "red"
+    }
 
      //-----edit-----//
      let taskEdit = document.createElement("div");
@@ -56,10 +99,7 @@ function createTask() {
       taskDeleteImg.alt = "Task_Delete_Icon"
       taskDelete.appendChild(taskDeleteImg);
 
-      //-----event for creating tasks-----//
-      
-
-      taskPriority.style.backgroundColor = "white";
+      //-----event for priority of tasks-----//
 
       taskPriority.addEventListener("click", function() {
         if (this.style.backgroundColor == "white") {
@@ -74,35 +114,6 @@ function createTask() {
     });
 }
 
-
-// -----this is code for making hovering preview of next priority colour, this is for the next 2 blocks of code, which include the mouseover and mouseout events.-----// 
-
-
-// priorityBtn.addEventListener("mouseover", function() {
-//     if (this.style.backgroundColor == "white") {
-//         this.style.backgroundColor = "lime";
-//     } else if (this.style.backgroundColor == "lime") {
-//         this.style.backgroundColor = "orange";
-//     } else if (this.style.backgroundColor == "orange") { 
-//         this.style.backgroundColor = "red";
-//     } else if (this.style.backgroundColor == "red") {
-//         this.style.backgroundColor = "white";
-//     }
-
-
-// })
-
-// priorityBtn.addEventListener("mouseout", function() {
-//     if (this.style.backgroundColor == "white") {
-//         this.style.backgroundColor = "red"
-//     } else if (this.style.backgroundColor == "lime") {
-//         this.style.backgroundColor = "white"
-//     } else if (this.style.backgroundColor == "orange") {
-//         this.style.backgroundColor = "lime"
-//     } else if (this.style.backgroundColor == "red") {
-//         this.style.backgroundColor = "orange" 
-//     }
-// })
 
 
 
