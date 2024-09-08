feade43839ce83130867479a313dd31d54c56ce7
hxcrry <harryrobbo77@outlook.com>
2024-09-08 14:02:08 +0100

    added a few more information things

--- projects/weatherApp/script.js
+++ projects/weatherApp/script.js
@@ -1,17 +1,22 @@
 const btn = document.querySelector("button");
 const weatherIcon = document.getElementById("weatherIcon");
+const searchLocation = document.getElementById("location");
 const conditions = document.getElementById("conditions");
+const description = document.getElementById("description");
+const temp = document.getElementById("temp");
 const dew = document.getElementById("dew");
 const feelsLike = document.getElementById("feelsLike");
 const humidity = document.getElementById("humidity");
-
+const UVIndex = document.getElementById("UVIndex");
+const precipitation = document.getElementById("precipitation");
 
 let userLocation;
-userLocation = "England"
-let response;
+userLocation = "london"
 
-btn.addEventListener("click", async function() {
-    fetch (`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userLocation}?key=A7F4BSEBQN5MBP9CX6QRGQQUQ`)
+
+
+btn.addEventListener("click", function() {
+    fetch (`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userLocation}?unitGroup=uk&key=A7F4BSEBQN5MBP9CX6QRGQQUQ`)
     .then (
         function(response) {
             return(response.json())
@@ -20,11 +25,14 @@ btn.addEventListener("click", async function() {
         function(responseJSON) {
             console.log(responseJSON)
             // weatherIcon.src = responseJSON.currentConditions.icon
+            searchLocation.innerHTML = "Location: " + responseJSON.resolvedAddress;
             conditions.innerHTML = "Conditions: " + responseJSON.currentConditions.conditions;
-            dew.innerHTML = "Dew: " + responseJSON.currentConditions.dew;
-            feelsLike.innerHTML = "Feels Like: " + responseJSON.currentConditions.feelslike;
-            humidity.innerHTML = "Conditions: " + responseJSON.currentConditions.humidity;
-            
+            description.innerHTML = "Description: " + responseJSON.description;
+            temp.innerHTML = "Temperature: " + responseJSON.currentConditions.temp + "°C"
+            feelsLike.innerHTML = "Feels Like: " + responseJSON.currentConditions.feelslike + "°C";
+            precipitation.innerHTML = "Precipitation: " + responseJSON.currentConditions.precip + " mm"
+            dew.innerHTML = "Dew Point: " + responseJSON.currentConditions.dew + "°";
+            humidity.innerHTML = "Humidity: " + responseJSON.currentConditions.humidity + "%";
         }
     )
     .catch ((err) => {
