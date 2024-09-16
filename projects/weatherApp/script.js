b057c34c68f9fcf5aa2703cdd95ed8499f6b5bbc
hxcrry <harryrobbo77@outlook.com>
2024-09-16 17:07:18 +0100

    re done desgin and added new data

--- projects/weatherApp/script.js
+++ projects/weatherApp/script.js
@@ -11,6 +11,16 @@ const humidity = document.getElementById("humidity");
 const UVIndex = document.getElementById("UVIndex");
 const precipitation = document.getElementById("precipitation");
 const locationInput = document.getElementById("locationInput");
+const cloudCover = document.getElementById("cloudCover");
+const precipProb = document.getElementById("precipProb");
+const precipType = document.getElementById("precipType");
+const pressure = document.getElementById("pressure");
+const sunrise = document.getElementById("sunrise");
+const sunset = document.getElementById("sunset");
+const visibility = document.getElementById("visibility");
+const windGust = document.getElementById("windGust");
+const windSpeed = document.getElementById("windSpeed");
+const windDirection = document.getElementById("windDirection");
 
 let userLocation;
 let tempC;
@@ -33,7 +43,7 @@ searchBtn.addEventListener("click", function(event) {
         userLocation = locationInput.value;
     };
     
-    fetch (`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userLocation}?unitGroup=uk&key=A7F4BSEBQN5MBP9CX6QRGQQUQ`)
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userLocation}?unitGroup=uk&key=A7F4BSEBQN5MBP9CX6QRGQQUQ`)
     .then (
         (response) => {
             return response.json()
@@ -55,23 +65,50 @@ searchBtn.addEventListener("click", function(event) {
             feelsLikeF = (feelsLikeC * 9/5 + 32);
             dewF = (dewC * 9/5) + 32;
             
-            searchLocation.innerHTML = "Location: " + responseJSON.resolvedAddress;
+            searchLocation.innerHTML = responseJSON.resolvedAddress;
 
-            conditions.innerHTML = "Current Weather: " + responseJSON.currentConditions.conditions;
+            conditions.innerHTML = responseJSON.currentConditions.conditions;
 
-            description.innerHTML = "Description: " + responseJSON.description;
+            description.innerHTML = responseJSON.description;
 
-            temp.innerHTML = "Temperature: " + tempC + "°C";
+            temp.innerHTML = tempC + "°C";
 
-            UVIndex.innerHTML = "UV Index: " + responseJSON.currentConditions.uvindex;
+            UVIndex.innerHTML = responseJSON.currentConditions.uvindex;
 
-            feelsLike.innerHTML = "Feels Like: " + feelsLikeC + "°C";
+            feelsLike.innerHTML = feelsLikeC + "°C";
 
-            precipitation.innerHTML = "Precipitation: " + responseJSON.currentConditions.precip + " mm"
+            precipitation.innerHTML = responseJSON.currentConditions.precip + " mm"
 
-            dew.innerHTML = "Dew Point: " + dewC + "°C";
+            dew.innerHTML = dewC + "°C";
 
-            humidity.innerHTML = "Humidity: " + responseJSON.currentConditions.humidity + "%";
+            humidity.innerHTML = responseJSON.currentConditions.humidity + "%";
+
+            cloudCover.innerHTML = responseJSON.currentConditions.cloudcover + "%";
+
+            precipProb.innerHTML = responseJSON.currentConditions.precipprob + "%";
+
+            // if (responseJSON.currentConditions.preciptype == null) {
+            //     precipType.innerHTML = "None"
+            // } else {
+            //     precipType.innerHTML = responseJSON.currentConditions.preciptype;
+            // }
+
+            //----------- !!!!!! this causes error because there is no precip so preciptype = null, need to find a way of making it say None if there is no precip !!!!!!!!!! -----------// also need to add button in top right corner to make days change
+            
+
+            pressure.innerHTML = responseJSON.currentConditions.pressure + "hPa";
+
+            sunrise.innerHTML = responseJSON.currentConditions.sunrise + " (local time)";
+
+            sunset.innerHTML = responseJSON.currentConditions.sunset + " (local time)";
+
+            visibility.innerHTML = responseJSON.currentConditions.visibility + " miles";
+
+            windGust.innerHTML = responseJSON.currentConditions.windgust + " mph";
+
+            windSpeed.innerHTML = responseJSON.currentConditions.windspeed + " mph";
+
+            windDirection.innerHTML = responseJSON.currentConditions.winddir + "°";
 
             // // if statement for background depending on the weather conditions
             // if (icon = "snow") {
@@ -96,15 +133,15 @@ changeBtn.addEventListener("click", function(event) {
 
     if (isCelsius % 2 === 0) {
          //even and true so change to farenheit
-        temp.innerHTML = "Temperature: " + tempF.toFixed(1) + "°F";
-        feelsLike.innerHTML = "Feels Like: " + feelsLikeF.toFixed(1) + "°F"
-        dew.innerHTML = "Dew Point: " + dewF.toFixed(1) + "°F"
+        temp.innerHTML = tempF.toFixed(1) + "°F";
+        feelsLike.innerHTML = feelsLikeF.toFixed(1) + "°F"
+        dew.innerHTML = dewF.toFixed(1) + "°F"
         changeBtn.innerHTML = "°F";
     } else {
         //odd and false so change to celsius
-        temp.innerHTML = "Temperature: " + tempC.toFixed(1) + "°C";
-        feelsLike.innerHTML = "Feels Like: " + feelsLikeC.toFixed(1) + "°C"
-        dew.innerHTML = "Dew Point: " + dewC.toFixed(1) + "°C"
+        temp.innerHTML = tempC.toFixed(1) + "°C";
+        feelsLike.innerHTML = feelsLikeC.toFixed(1) + "°C"
+        dew.innerHTML = dewC.toFixed(1) + "°C"
         changeBtn.innerHTML = "°C";
     };
 })
