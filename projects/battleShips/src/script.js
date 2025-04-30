e7f2196fd4490fce1ea77ef60cf6af0fdc970258
hxcrry <harryrobbo77@outlook.com>
2025-04-30 11:51:07 +0100

    fix: made it so the boats only calculate horizontal or vertical, but then if it cannot fit one way, it places the other way around, or if it wont fit at all it will not place anything there and go back through the loop and find another position on the board

--- projects/battleShips/src/script.js
+++ projects/battleShips/src/script.js
@@ -62,41 +62,75 @@ class GameBoard {
             return Math.floor(Math.random() * max)
         }
 
-        while (canPlace === false) {
-            let randomPos = getRandomNum(99) // use random num to call a random coordinate
-            let randomDirection = getRandomNum(1)
+        while (canPlace == false) {
+            let horizontalCheck;
+            let horizontalCheckAdd; // true = added, false = sutracted
 
-            let xAxisPos = this.board[randomPos][0][0] // if randomPos = 50, will be the 6th row because 0 is the 1st, returns 6
-            let yAxisPos = this.board[randomPos][0][1] // if randomPos = 50, will be the 6th column because 0 is the 1st, returns F
+            let verticalCheck;
+            let verticalCheckAdd;// true = added, false = sutracted
+            
+            let randomPos = getRandomNum(99)// use random num to call a random coordinate
+            let randomDirection = getRandomNum(2)
 
-            let horizontalCheck = (xAxisPos === this.board[randomPos + (shipSize - 1)][0][0]) // checks to make sure the row has remained the same, used to check ships placed horizontally dont go over into other columns
-            let verticalCheck = (yAxisPos === this.board[randomPos + (shipSize - 1)][0][1]) // checks to make sure the column has remained the same, used to check ships placed vertically dont go over into other rows 
+            let xAxisPos = this.board[randomPos] // if randomPos = 50, will be the 6th row because 0 is the 1st, returns 6
+            let yAxisPos = this.board[randomPos] // if randomPos = 50, will be the 6th column because 0 is the 1st, returns F
 
+            // canPlace = true
+            // let endPos = this.board[randomPos + ((shipSize - 1) * 10)]
+            // horizontalCheck = (xAxisPos[0][1] === endPos[0][1]) 
+            // return horizontalCheck
             if (randomDirection == 0) { // horizontal check, y axis will stay the same
-                if (horizontalCheck) {
+                if ((randomPos + ((shipSize - 1) * 10)) <= 99) { // checks if ship can fit horizontally and does not spill over the edge by adding working out the next correct space
+                    let endPos = this.board[randomPos + ((shipSize - 1) * 10)]
+                    horizontalCheck = (xAxisPos[0][1] === endPos[0][1]) 
+                    horizontalCheckAdd = true
+    
+                } else if (randomPos - ((shipSize - 1) * 10) >= 0) { // cannot fit by adding so checks if it would fit by subtracting, which it should always and (as i currently understand) cannot fail if the other fails. 
+                    let endPos = this.board[randomPos - ((shipSize - 1) * 10)]
+                    horizontalCheck = (xAxisPos[0][1] === endPos[0][1]) 
+                    horizontalCheckAdd = false
+                }
+
+                if (horizontalCheck && (horizontalCheckAdd === true)) {
                     canPlace = true
-                    message = "HORIZONTAL"
+                    message = "HORIZONTAL + "
                     for (let i = 0; i < shipSize; i++) {
                         let next = 10 * i
                         fullPos.push(this.board[randomPos + next])
                     }
-                } else {
+
+                } else if (horizontalCheck && horizontalCheckAdd === false) {
                     canPlace = true
-                    message = "HORIZONTAL"
-                    return `couldnt place ${message} at ${this.board[randomPos]}`
+                    message = "HORIZONTAL - " 
+                    for (let i = 0; i < shipSize; i++) {
+                        let next = 10 * i
+                        fullPos.push(this.board[randomPos - next])
+                    }
                 }
 
             } else if (randomDirection == 1) { // vertical check, x axis will stay the same
-                if (verticalCheck) {
+                if (randomPos + (shipSize - 1) <= 99) {// checks that ships dont spill over out of the board
+                    let endPos = this.board[randomPos + (shipSize - 1)]
+                    verticalCheck = (yAxisPos[0][0] === endPos[0][0])
+                    verticalCheckAdd = true
+                } else if (randomPos - (shipSize - 1) >= 0) { // if it will spill out, it subtracts and places ship the other vertical way
+                    let endPos = this.board[randomPos - (shipSize - 1)]
+                    verticalCheck = (yAxisPos[0][0] === endPos[0][0])
+                    verticalCheckAdd = false
+                }
+
+                if (verticalCheck && verticalCheckAdd === true) {
                     canPlace = true
-                    message = "VERTICAL"
+                    message = "VERTICAL + "
                     for (let i = 0; i < shipSize; i++) {
                         fullPos.push(this.board[randomPos + i])
                     }
-                } else {
+                } else if (verticalCheck && verticalCheckAdd === false) {
                     canPlace = true
-                    message = "VERTICAL"
-                    return `couldnt place ${message} at ${this.board[randomPos]}`
+                    message = "VERTICAL - "
+                    for (let i = 0; i < shipSize; i++) {
+                        fullPos.push(this.board[randomPos - i])
+                    }
                 }
             }
         }
