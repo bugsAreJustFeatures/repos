0a96ec78212a87370f1293aade4971a15eedd1ca
hxcrry <harryrobbo77@outlook.com>
2025-09-07 16:42:53 +0100

    changed DOM creation of form into jsx and with state

--- projects/wheresWaldo/frontend/src/HomePage/HomePage.jsx
+++ projects/wheresWaldo/frontend/src/HomePage/HomePage.jsx
@@ -1,103 +1,75 @@
-import styles from "./HomePage.module.css"
-import image from "../assets/waldo1.jpg"
-import { useState } from "react";
+    import styles from "./HomePage.module.css"
+    import image from "../assets/waldo1.jpg"
+    import { useState } from "react";
 
-export default function HomePage() {
+    export default function HomePage() {
 
-    // state variables
-    const [isThereBox, setIsThereBox] = useState(false);
+        // state variables
+        const [isThereBox, setIsThereBox] = useState(false);
+        const [pageClickX, setPageClickX] = useState(null);
+        const [pageclickY, setPageClickY] = useState(null);
+        const [imageClickX, setImageClickX] = useState(null);
+        const [imageClickY, setImageClickY] = useState(null);
 
-    // function that handles the click on image, displays box where user clicked
-    function handleImageClick(e) {
-
-        // --NOTE-- //
-        // if the page is resized after the image has been clicked then the box will move since
-        // im placing it on the page and using the page coords to place it but dont 
-        // worry since there will only be one box showing at a time and i will have 
-        // already have recorded where the user has clicked on the image which is all
-        // i really need, also user can just re-click where they want to if they 
-        // resize page 
-
-        // amount of margin (offset) being appied to image itself
-        const offsetTop = e.target.offsetTop; 
-        const offsetLeft = e.target.offsetLeft;
+        // function that handles the click on image, displays box where user clicked
         
-        // click coords on user's page
-        const pageX = e.clientX; 
-        const pageY = e.clientY;
+        function handleImageClick(e) {
 
-        // click coords on image (what i want to record in db)
-        const clickOnImageX = pageX - offsetLeft; 
-        const clickOnImageY = pageY - offsetTop; 
+            // --NOTE-- //
+            // if the page is resized after the image has been clicked then the box will move since
+            // im placing it on the page and using the page coords to place it but dont 
+            // worry since there will only be one box showing at a time and i will have 
+            // already have recorded where the user has clicked on the image which is all
+            // i really need, also user can just re-click where they want to if they 
+            // resize page 
 
-        // get rid of old box when new one is placed
-        if (isThereBox) {
-            const oldBox = document.getElementById(styles.selectCharacterBox);
-            e.target.parentElement.removeChild(oldBox);
+            // amount of margin (offset) being appied to image itself
+            const offsetTop = e.target.offsetTop; 
+            const offsetLeft = e.target.offsetLeft;
+            
+            // click coords on user's page
+            const clickOnPageX = e.clientX; 
+            const clickOnPageY = e.clientY;
+
+            // click coords on image (what i want to record in db)
+            const clickOnImageX = clickOnPageX - offsetLeft; 
+            const clickOnImageY = clickOnPageY - offsetTop; 
+
+            // state updates
+            setPageClickX(clickOnPageX);
+            setPageClickY(clickOnPageY);
+            setImageClickX(clickOnImageX);
+            setImageClickY(clickOnImageY);
+            setIsThereBox(true);
         };
 
-        // box that is placed where user has clicked to display menu to select character
-        const selectCharacterBox = document.createElement("div"); // define element
-        selectCharacterBox.id = styles.selectCharacterBox; // give css module id
-        selectCharacterBox.style.left = `${pageX}px`; // uses the px's from the left side that the user clicked, as the margin
-        selectCharacterBox.style.top = `${pageY}px`; // same above but from the top
-        e.target.parentElement.appendChild(selectCharacterBox); // add element into DOM and as a child to the parent (waldoImageContainer)
+        // form that handles sumbitting character selection form
+        function handleForm(e) {
+            e.preventDefault(); 
 
-        // create form that has an event listener and select options
-        const selectCharacterForm = document.createElement("form");
-        selectCharacterForm.id = styles.selectCharacterForm;
-        selectCharacterForm.addEventListener("submit", (e) => {handleForm(e)});
+            console.log(`B - X: ${imageClickX}, Y: ${imageClickY}`);
+            console.log(e.target)
+        }
 
-        // select field that houses the options (drop down menu)
-        const selectField = document.createElement("select");
-        selectField.id = styles.selectField;
 
-        // submit button that is used to submit the form
-        const sumbitBtn = document.createElement("button");
-        sumbitBtn.id = styles.submitBtn;
-        sumbitBtn.type = "sumbit";
-        sumbitBtn.innerHTML = "Confirm Character";
+        return (
+            <div id={styles.wrapper}>
+                <div id={styles.waldoImageContainer}>
+                    <img onClick={handleImageClick} src={image} alt="a where's waldo image" id={styles.waldoImage} />
 
-        // add an option for each character into the select field via looping
-        const characters = ["waldo", "wally"];
-        characters.forEach((character, key) => {
-            // new Option(text, value, defaultSelected, selected)
-            const option = new Option(character, character, false, false);
-            option.id = styles[`${character}Option`]; // give unique name from character such as styles.waldoOption
-            selectField.appendChild(option);
-        });
+                    {isThereBox && (
+                        <div id={styles.selectCharacterBox} style={{left: `${pageClickX}px`, top: `${pageclickY}px`}}>
+                            <form id={styles.selectCharacterForm} onSubmit={(e) => {handleForm(e)}}>
+                                <select name="selectField" id={styles.selectField}>
+                                    <option value="waldo" id={styles.waldoOption}> Waldo </option>
+                                    <option value="wally" id={styles.wallyOption}> Wally </option>
+                                </select>
 
-        // adding to DOM in the correct order
-        selectCharacterForm.appendChild(selectField);
-        selectCharacterForm.appendChild(sumbitBtn)
-        selectCharacterBox.appendChild(selectCharacterForm);
-
-        // debugging 
-
-        // console.logs
-        // console.log("x-axis on image: ", clickOnImageX)
-        // console.log("y-axis on image: ", clickOnImageY)
-        // console.log("pageX: ", pageX)
-        // console.log("pageY: ", pageY)
-        // console.log(e)
-
-        // update state
-        setIsThereBox(true);
+                                <button type="submit">Confirm Character</button>
+                            </form>
+                        </div>
+                    )}
+                </div>
+            </div>  
+        )
     };
-
-    // form that handles sumbitting character selection form
-    function handleForm(e) {
-        e.preventDefault();
-
-        console.log(e.target)
-    }
-
-
-    return (
-        <div id={styles.wrapper}>
-            <div id={styles.waldoImageContainer}>
-                <img onClick={handleImageClick} src={image} alt="a where's waldo image" id={styles.waldoImage} />
-            </div>
-        </div>  
-    )
-};
