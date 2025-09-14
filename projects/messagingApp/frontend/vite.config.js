3c009a43eaeaa160a11259c0c86491566ea32396
hxcrry <harryrobbo77@outlook.com>
2025-09-14 13:12:57 +0100

    fix not loading page

--- projects/messagingApp/frontend/vite.config.js
+++ projects/messagingApp/frontend/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const PORT = process.env.PORT || 3000;
+
 // https://vite.dev/config/
 export default defineConfig({
+  root: __dirname,
   plugins: [react()],
+  build: {
+    outDir: "../dist",
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: `http://localhost:${PORT}`,
+        changeOrigin: true,
+      },
+    },
+  },
 })
